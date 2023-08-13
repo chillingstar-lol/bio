8e66aafd5d0f4aa17de348fa5fd5ed47a7585e07
ChillingStar <72331969+chillingstar@users.noreply.github.com>
2023-08-13 20:25:59 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function announceDiscord() {
+    alert("My discord is: chillingtwo")
+}
+
+function announceGitHub() {
+    alert("My GitHub is: chillingstar")
+}
+
+function redirectBallsClicker() {
+    window.location.href = "https://ballsclicker.chillingstar.lol"
+}
+
+function redirectSenerity() {
+    // window.location.href = "https://senerity.xyz"
+    alert("Coming soon...")
+}
